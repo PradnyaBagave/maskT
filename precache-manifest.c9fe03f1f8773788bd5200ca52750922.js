247ac4a145946fd1ab2987b559815dfa29628913
romafashionentertainment <41669215+romafashionentertainment@users.noreply.github.com>
2018-12-14 00:11:15 +0530

    'deploy'

---
+++ precache-manifest.c9fe03f1f8773788bd5200ca52750922.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fd24808fe105b5bd0ff",
+    "url": "/js/chunk-52fabea2.f0df09fc.js"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "223cbb4444e46bb1720b",
+    "url": "/js/chunk-5790bd50.802af4be.js"
+  },
+  {
+    "revision": "80d1381e27fc2260767c",
+    "url": "/js/chunk-0bfccfa5.dd10cbc4.js"
+  },
+  {
+    "revision": "82364c524ad2849a5543",
+    "url": "/js/chunk-2d0d6d35.93547eae.js"
+  },
+  {
+    "revision": "5c37d5ffb56ef0e277ec",
+    "url": "/js/chunk-2d2086b7.a1c0eb99.js"
+  },
+  {
+    "revision": "1a11822d191cc5cee936",
+    "url": "/js/chunk-2d217357.2598af1c.js"
+  },
+  {
+    "revision": "06f317d87eb070439cef",
+    "url": "/js/chunk-4ba46e74.3140cdd7.js"
+  },
+  {
+    "revision": "1a3b51c6b1caaa2bfa6a",
+    "url": "/js/chunk-vendors.223dd562.js"
+  },
+  {
+    "revision": "11863f700026d68b7481",
+    "url": "/js/chunk-2d0b3289.b2133bdd.js"
+  },
+  {
+    "revision": "f605d9112048fd93bc6e",
+    "url": "/js/chunk-704fe663.53ee8f27.js"
+  },
+  {
+    "revision": "48c46a8682616338f3aa",
+    "url": "/js/chunk-9bce2a3c.9ee04e84.js"
+  },
+  {
+    "revision": "62165cd1217e97426860",
+    "url": "/js/chunk-e6f9d2ae.1e43fab4.js"
+  },
+  {
+    "revision": "416725b0fb05403d4ee8",
+    "url": "/js/chunk-fee37f4e.cbc50a5c.js"
+  },
+  {
+    "revision": "4c4776f2c7a63c93ba40",
+    "url": "/js/app.b2959fff.js"
+  },
+  {
+    "revision": "9edabd6a994890779ed571873d807391",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c4776f2c7a63c93ba40",
+    "url": "/css/app.07874014.css"
+  }
+];
